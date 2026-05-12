440dc57013c929dd7f30d4d7c3bfff561ac21ab5
wangben <196322385@qq.com>
2026-05-12 15:21:13 +0800

    添加Minio配置和依赖

---
+++ src/main/java/com/lvai/config/MinioConfig.java
@@ -0,0 +1,28 @@
+package com.lvai.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endPoint;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endPoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
